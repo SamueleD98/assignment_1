63643db679ba92f1919ba221ff50eea0f01a3b46
samuele <depalo.samuele@gmail.com>
2022-11-26 11:20:07 +0100

    cancal a mistake

--- scripts/state_machine.py
+++ scripts/state_machine.py
@@ -157,7 +157,96 @@ class Recharge(State):
         return 'done'
 
 
+# class Recharge(State):
+#     def __init__(self):
+#         State.__init__(self,
+#                              outcomes=['done'],
+#                              )
+#
+#
+#     def execute(self, userdata):
+#         global battery_is_low
+#         print(' ')
+#         rospy.loginfo('Executing state RECHARGE ')
+#
+#         get_recharge_room = OICommandGoal()
+#         get_recharge_room.command = 'recharge_room'
+#         OI_client.send_goal(get_recharge_room)
+#         while(OI_client.get_state() != 3):
+#             if self.preempt_requested():
+#                 OI_client.cancel_all_goals()
+#                 self.service_preempt()
+#                 return 'battery_low'
+#         OI_client.wait_for_result()
+#         recharge_room = OI_client.get_result().res
+#
+#         rospy.loginfo('Going to '+ recharge_room)
+#
+#         #CALL PLANNER
+#         # while(planner_client.get_state() != 3):
+#         #     if self.preempt_requested():
+#         #         planner_client.cancel_all_goals()
+#         #         self.service_preempt()
+#         #         return 'battery_low'
+#         # planner_client.wait_for_result()
+#         # plan = planner_client.get_result().list
+#
+#         #CALL CONTROLLER
+#         # while(controller_client.get_state() != 3):
+#         #     if self.preempt_requested():
+#         #         controller_client.cancel_all_goals()
+#         #         self.service_preempt()
+#         #         return 'battery_low'
+#         # controller_client.wait_for_result()
+#         # done_control = controller_client.get_result().list
+#
+#
+#         # WHAT DOES THE RECHARGING????
+#
+#         # recharge = OICommandGoal()
+#         # recharge.command = 'recharge'
+#         # OI_client.send_goal(recharge)
+#         # while(OI_client.get_state() != 3):
+#         #     if self.preempt_requested():
+#         #         OI_client.cancel_all_goals()
+#         #         self.service_preempt()
+#         #         #battery_is_low = False
+#         #         rospy.loginfo('Recharged!')
+#         #         return 'done'
+#         # OI_client.wait_for_result()
+#
+#         battery_is_low = False
+#         rospy.loginfo('Recharged!')
+#         return 'done'
 
+
+
+# gets called when ANY child state terminates
+def child_term_cb(outcome_map):
+  # terminate ALL the child states
+  return True
+
+# gets called when ALL child states are terminated
+def out_cb_monitoring(outcome_map):
+   if outcome_map['EXECUTE'] == 'preempted':
+      return 'battery_low'
+   else:
+      return 'monitoring_done'
+def out_cb_recharge(outcome_map):
+      return 'recharge_done'
+
+def monitor_cb(ud, msg):
+    global battery_is_low
+    #string =
+    rospy.loginfo('BATTERY STATUS UPDATE: is battery low? --> ' + str(msg.data))
+    # rospy.loginfo('BATTERY STATUS UPDATE: is battery low? --> ')
+    # rospy.loginfo(msg.data)
+    if battery_is_low == msg.data :
+        #rospy.loginfo('BATTERY STATUS UPDATE: is battery low? --> '+msg.data)
+        return True
+    #print('change status!!')
+    battery_is_low = msg.data
+    return False
 
 
 def main():
@@ -166,33 +255,56 @@ def main():
 
     # Create a SMACH state machine
     sm_main = StateMachine([])
+
+    # Open the container
     with sm_main:
         #---------- MAPPING STATE ----------
         StateMachine.add('MAPPING', Mapping(),
                                transitions={'map_loaded':'MONITORING'})
 
-        #---------- MONITORING STATE ----------
-        sm_monitoring = StateMachine(outcomes=['monitoring_done','battery_low'])
+        #---------- MONITORING (CONCURRENCE) STATE ----------
+        sm_monitoring = Concurrence(outcomes=['monitoring_done','battery_low'],
+                                   default_outcome='monitoring_done',
+                                   child_termination_cb = child_term_cb,
+                                   outcome_cb = out_cb_monitoring)
         with sm_monitoring:
+            Concurrence.add('CHECK_BATTERY_STATUS',
+                                MonitorState("/battery_status", Bool, monitor_cb))
+
+            sm_monitoring_exe = StateMachine(outcomes=['monitoring_done', 'preempted'])
+            with sm_monitoring_exe:
                 StateMachine.add('MOVE_TO_MONITOR', Move(type = 'monitor'),
                                  transitions={'done':'MONITOR',
-                                              'preempted':'battery_low'})
+                                              'preempted':'preempted'})
                 StateMachine.add('MONITOR', Monitor(),
                                  transitions={'done':'monitoring_done',})
                                               #'preempted':'preempted'})
-                                              
+            Concurrence.add('EXECUTE',
+                            sm_monitoring_exe)
+
         StateMachine.add('MONITORING', sm_monitoring,
                                transitions={'monitoring_done':'MONITORING',
                                             'battery_low':'RECHARGING'})
-        #---------- RECHARGE STATE ----------
-        sm_recharge = StateMachine(outcomes=['recharge_done'])
+
+        #---------- RECHARGE (CONCURRENCE) STATE ----------
+        sm_recharge = Concurrence(outcomes=['recharge_done'],
+                                   default_outcome='recharge_done',
+                                   child_termination_cb = child_term_cb,
+                                   outcome_cb = out_cb_recharge)
         with sm_recharge:
+            Concurrence.add('CHECK_BATTERY_STATUS',
+                                MonitorState("/battery_status", Bool, monitor_cb))
+
+            sm_recharging_exe = StateMachine(outcomes=['recharge_done'])
+            with sm_recharging_exe:
                 StateMachine.add('MOVE_TO_RECHARGE', Move(type = 'recharge'),
                                  transitions={'done':'RECHARGE',
                                               'preempted':'recharge_done'})
                 StateMachine.add('RECHARGE', Recharge(),
-                                 transitions={'done':'recharge_done',})
-                                              #'preempted':'preempted'})
+                                 transitions={'done':'recharge_done'})
+
+            Concurrence.add('EXECUTE',
+                                sm_recharging_exe)
 
         StateMachine.add('RECHARGING', sm_recharge,
                                transitions={'recharge_done':'MONITORING'})
